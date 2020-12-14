44a7c4b307a6920d82ff7dfe40f7f8dda3a42b40
Iyemay <iyemay96@gmail.com>
2020-12-14 11:26:29 -0600

    Put the text of the question, help and dynamic answers

--- src/components/QuestionsComponent.js
+++ src/components/QuestionsComponent.js
@@ -6,34 +6,34 @@ export default class QuestionsComponent extends Component {
             this.props.visible ?
                 (<div className="help-answer">
                     <div className="help-container">
-                        <p>Consider all the money your household made and spent.</p>
+                        <p>{this.props.helpChallenge}</p>
                     </div>
 
                     <div id="question-answers-container" className="content-container">
                         <p className="question-size">
-                            In the last 12 months, my household <span className="strong-word">spent</span>...
+                            {this.props.textChallenge}
                         </p>
 
                         <div className="answer-container">
                             <div className="answer-box">
                                 <div className="answer-number">5</div>
-                                <p className="answer-option">Much less than our income</p>
+                                <p className="answer-option">{this.props.answersChallenge[0].text}</p>
                             </div>
                             <div className="answer-box">
                                 <div className="answer-number">4</div>
-                                <p className="answer-option">A little less than our income</p>
+                                <p className="answer-option">{this.props.answersChallenge[1].text}</p>
                             </div>
                             <div className="answer-box">
                                 <div className="answer-number">3</div>
-                                <p className="answer-option">About equal to our income</p>
+                                <p className="answer-option">{this.props.answersChallenge[2].text}</p>
                             </div>
                             <div className="answer-box">
                                 <div className="answer-number">2</div>
-                                <p className="answer-option">A little more than our income</p>
+                                <p className="answer-option">{this.props.answersChallenge[3].text}</p>
                             </div>
                             <div className="answer-box">
                                 <div className="answer-number">1</div>
-                                <p className="answer-option">Much more than our income</p>
+                                <p className="answer-option">{this.props.answersChallenge[4].text}</p>
                             </div>
                         </div>
                     </div>
--- src/components/QuizComponent.js
+++ src/components/QuizComponent.js
@@ -19,12 +19,27 @@ class QuizComponent extends Component {
         this.props.goToLast();
     }
 
+
     render () {
+
+        const challenges =  this.props.quiz.filter(function(challenge) {
+            return challenge.id === 'challenge1';
+        });
+
+        console.log(challenges);
+        console.log("Help " + challenges[0].help);
+        console.log("Text " + challenges[0].text);
+        console.log("Answers " + (challenges[0].answers[0].text));
+
         return (
             <div>
                 <StartComponent goToQuestions={this.goToQuestions} visible={this.props.startComponentVisibility}/>
+
                 <QuestionsComponent goToStart={this.goToStart} goToLast={this.goToLast}
-                                    visible={this.props.questionsComponentVisibility}/>
+                                    visible={this.props.questionsComponentVisibility}
+                                    textChallenge={challenges[0].text}
+                                    helpChallenge={challenges[0].help}
+                                    answersChallenge={challenges[0].answers}/>
                 <LastComponent visible={this.props.lastComponentVisibility}/>
             </div>
         );
@@ -35,7 +50,8 @@ function mapStateToProps(state) {
     return {
         startComponentVisibility: state.startComponentVisibility,
         questionsComponentVisibility: state.questionsComponentVisibility,
-        lastComponentVisibility: state.lastComponentVisibility
+        lastComponentVisibility: state.lastComponentVisibility,
+        quiz: state.quiz
     };
 }
 
